9d9b89a3dd3d4972874a49f88d4cb34f56d2d21b
Jose <joseantgv@gmail.com>
2012-01-08 06:01:36 +0100

    Chart

--- LoadSensingApp/src/com/loadsensing/app/Chart.java
+++ LoadSensingApp/src/com/loadsensing/app/Chart.java
@@ -1,5 +1,9 @@
 package com.loadsensing.app;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -15,6 +19,7 @@ public class Chart extends DashboardActivity {
 
 	private static final String DEB_TAG = "Json_Android";
 	private String SERVER_HOST = "http://viuterrassa.com/Android/getValorsGrafic.php";
+	private String CHART_URL = "http://chart.apis.google.com/chart";
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,8 +27,7 @@ public class Chart extends DashboardActivity {
 		setContentView(R.layout.chart);
 		WebView googleChartView = (WebView) findViewById(R.id.chart);
 		// URL example
-		// String mUrl =
-		// "http://chart.apis.google.com/chart?cht=p3&chd=t:30,60,10&chs=250x100&chl=cars|bikes|trucks";
+		// String mUrl ="http://chart.apis.google.com/chart?cht=p3&chd=t:30,60,10&chs=250x100&chl=cars|bikes|trucks";
 
 		SharedPreferences settings = getSharedPreferences("LoadSensingApp",
 				Context.MODE_PRIVATE);
@@ -35,17 +39,26 @@ public class Chart extends DashboardActivity {
 			String jsonString = JsonClient.connectString(address);
 
 			// Convertim la resposta string a un JSONArray
-			JSONArray llistaXarxesArray = new JSONArray(jsonString);
+			JSONArray ValorsGrafica = new JSONArray(jsonString);
 
-			// HashMap<String, String> xarxa = null;
+			HashMap<String, String> valorHashMap = new HashMap<String, String>();
+			List<HashMap<String, String>> valorsURL = new ArrayList<HashMap<String, String>>();
 
-			for (int i = 0; i < llistaXarxesArray.length(); i++) {
-				// xarxa = new HashMap<String, String>();
-				JSONObject xarxaJSON = new JSONObject();
-				xarxaJSON = llistaXarxesArray.getJSONObject(i);
+			JSONObject Valors = new JSONObject();
+			Valors = ValorsGrafica.getJSONObject(0);
 
-				// xarxaJSON.getString("Lat");
+			String grafica = Valors.getString("ValorsGrafica");
+			Log.i(DEB_TAG, "String " + grafica);
 
+			JSONArray ValorGrafica = new JSONArray(grafica);
+			for (int i = 0; i < ValorGrafica.length(); i++) {
+				JSONObject Valor = new JSONObject();
+				Valor = ValorGrafica.getJSONObject(i);
+
+				valorHashMap.put("date", Valor.getString("date"));
+				valorHashMap.put("value", Valor.getString("value"));
+				valorsURL.add(valorHashMap);
+				//Log.i(DEB_TAG, "String " + valorsURL.get(i).get("date"));
 			}
 
 		} catch (Exception e) {
