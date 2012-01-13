4c71fe73ccbeb07912385ece81f37a5e9de21b84
Jose <joseantgv@gmail.com>
2012-01-13 10:47:22 +0100

    Thumbnail

--- LoadSensingApp/src/com/loadsensing/app/LlistaXarxesActivity.java
+++ LoadSensingApp/src/com/loadsensing/app/LlistaXarxesActivity.java
@@ -26,14 +26,17 @@ import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.SimpleAdapter.ViewBinder;
 import android.widget.TextView;
 
 import com.loadsensing.client.JsonClient;
@@ -49,9 +52,11 @@ public class LlistaXarxesActivity extends ListActivity {
 		ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
 		SimpleAdapter adapter = new SimpleAdapter(this, list,
 				R.layout.xarxa_row_list_view, new String[] { "nom", "poblacio",
-						"sensors", "lat", "lon", "idXarxa", "nomThumbnail" }, new int[] {
-						R.id.text1, R.id.text2, R.id.text3, R.id.text4,
-						R.id.text5, R.id.text6, R.id.thumbnailxarxa});
+						"sensors", "lat", "lon", "idXarxa", "nomThumbnail" },
+				new int[] { R.id.text1, R.id.text2, R.id.text3, R.id.text4,
+						R.id.text5, R.id.text6, R.id.thumbnailxarxa });
+
+		adapter.setViewBinder(new setImageListView());
 
 		SharedPreferences settings = getSharedPreferences("LoadSensingApp",
 				Context.MODE_PRIVATE);
@@ -71,20 +76,21 @@ public class LlistaXarxesActivity extends ListActivity {
 				xarxa = new HashMap<String, String>();
 				JSONObject xarxaJSON = new JSONObject();
 				xarxaJSON = llistaXarxesArray.getJSONObject(i);
-				String nomImatge = obtenirNomImatge(xarxaJSON.getString("IdXarxa"));
-				Log.i(DEB_TAG,"nom: " + nomImatge);
-				xarxa.put("id", String.valueOf(i));				
+				int nomImatge = obtenirNomImatge(xarxaJSON
+						.getString("IdXarxa"));
+				Log.i(DEB_TAG, "nom: " + nomImatge);
+				xarxa.put("id", String.valueOf(i));
 				xarxa.put("poblacio", xarxaJSON.getString("Poblacio"));
 				xarxa.put("nom", xarxaJSON.getString("Nom"));
 				xarxa.put("idXarxa", xarxaJSON.getString("IdXarxa"));
 				xarxa.put("sensors", xarxaJSON.getString("Sensors"));
 				xarxa.put("lat", xarxaJSON.getString("Lat"));
 				xarxa.put("lon", xarxaJSON.getString("Lon"));
-				xarxa.put("nomThumbnail", nomImatge);
-				
+				xarxa.put("nomThumbnail", Integer.toString(nomImatge));
+
 				Log.d(DEB_TAG, xarxaJSON.getString("Poblacio"));
 				list.add(xarxa);
-				
+
 			}
 			setListAdapter(adapter);
 
@@ -111,20 +117,20 @@ public class LlistaXarxesActivity extends ListActivity {
 	public void goBack(View v) {
 		finish();
 	}
-	
-	public String obtenirNomImatge(String idXarxa)  {
-		
-		String nomImatge = "";
-		
+
+	public int obtenirNomImatge(String idXarxa) {
+
+		int nomImatge = 0;
+
 		switch (Integer.parseInt(idXarxa)) {
+		case 0:
+			nomImatge = R.drawable.sagradafamilia;
+			break;
 		case 1:
-			nomImatge = "sagradafamilia";
+			nomImatge = R.drawable.torreagbar;
 			break;
 		case 2:
-			nomImatge = "torreagbar";
-			break;
-		case 3:
-			nomImatge = "pontvalencia";
+			nomImatge = R.drawable.pontvalencia;
 			break;
 		default:
 			break;
@@ -147,8 +153,8 @@ public class LlistaXarxesActivity extends ListActivity {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.menu, menu);
 		return true;
-	}	
-	
+	}
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle item selection
@@ -172,6 +178,21 @@ public class LlistaXarxesActivity extends ListActivity {
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	public class setImageListView implements ViewBinder {
+		@Override
+		public boolean setViewValue(View view, Object data,
+				String textRepresentation) {
+			if ((view instanceof ImageView) & (data instanceof Bitmap)) {
+				ImageView iv = (ImageView) view;
+				Bitmap bm = (Bitmap) data;
+				iv.setImageBitmap(bm);
+				return true;
+			}
+
+			return false;
 		}
 	}
 }
