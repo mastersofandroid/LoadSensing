2869997b9a12c3f332e8ebf1a0c92810a50b7874
Jose <joseantgv@gmail.com>
2012-01-14 15:52:47 +0100

    QR

--- LoadSensingApp/src/com/loadsensing/app/QRActivity.java
+++ LoadSensingApp/src/com/loadsensing/app/QRActivity.java
@@ -18,23 +18,23 @@ public class QRActivity extends DashboardActivity {
 
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		switch (requestCode) {
-		case IntentIntegrator.REQUEST_CODE: {
-			if (resultCode != RESULT_CANCELED) {
-				IntentResult scanResult = IntentIntegrator.parseActivityResult(
-						requestCode, resultCode, data);
-				if (scanResult != null) {
-					String idsensor = scanResult.getContents();
-
-					Intent intent = new Intent();
-					intent.setClass(this.getApplicationContext(),
-							SingleSensorActivity.class);
-					intent.putExtra("idsensorselected", idsensor);
-					startActivity(intent);
-					finish();
+			case IntentIntegrator.REQUEST_CODE: {
+				if (resultCode != RESULT_CANCELED) {
+					IntentResult scanResult = IntentIntegrator.parseActivityResult(
+							requestCode, resultCode, data);
+					if (scanResult != null) {
+						String idsensor = scanResult.getContents();
+	
+						Intent intent = new Intent();
+						intent.setClass(this.getApplicationContext(),
+								SingleSensorActivity.class);
+						intent.putExtra("idsensorselected", idsensor);
+						startActivity(intent);
+						finish();
+					}
 				}
+				break;
 			}
-			break;
-		}
 		}
 	}
 }
